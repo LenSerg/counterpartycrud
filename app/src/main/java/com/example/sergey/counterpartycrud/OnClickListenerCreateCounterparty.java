40232dcd10ef5f1a9e5619ebe1b7f3648f58d5c2
Len Sergey <len.sergey.a@gmail.com>
2017-08-20 23:09:10 +0700

    Adding main and counterparty's edit form

---
+++ app/src/main/java/com/example/sergey/counterpartycrud/OnClickListenerCreateCounterparty.java
@@ -0,0 +1,41 @@
+package com.example.sergey.counterpartycrud;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+/**
+ * Created by sergey on 20.08.17.
+ */
+
+public class OnClickListenerCreateCounterparty implements View.OnClickListener {
+    @Override
+    public void onClick(View view) {
+        Context context = view.getRootView().getContext();
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final View formView = inflater.inflate(R.layout.counterparty_edit_form, null, false);
+
+        final EditText photoEditText = formView.findViewById(R.id.photoTextEdit);
+        final EditText nameEditText = formView.findViewById(R.id.nameTextEdit);
+        final EditText addressEditText = formView.findViewById(R.id.addressTextEdit);
+        final EditText phoneEditText = formView.findViewById(R.id.phoneTextEdit);
+        final EditText emailEditText = formView.findViewById(R.id.emailTextEdit);
+        final EditText websiteEditText = formView.findViewById(R.id.websiteTextEdit);
+        final EditText descriptionEditText = formView.findViewById(R.id.descriptionTextEdit);
+
+        new AlertDialog.Builder(context)
+                .setView(formView)
+                .setTitle("Create counterparty")
+                .setPositiveButton("Ok",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                            }
+                        }).show();
+    }
+}
